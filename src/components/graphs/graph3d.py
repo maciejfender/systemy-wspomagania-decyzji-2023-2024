bb6b93101e87da53dedcf20e74a98b9c765c648c
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2023-12-06 15:22:11 +0100

    poprawienie grafu 3d

--- src/components/graphs/graph3d.py
+++ src/components/graphs/graph3d.py
@@ -9,7 +9,7 @@ class Graph3D(tk.Frame):
         super().__init__(master, width=width, height=height)
         self.data = []
         self.scatter_or_plot = None
-        self.color = '#ff007f'
+        self.colors = ['#ff007f', '#001f3f', '#800080', '#01ff70', '#ff6300', '#00A86B']
         self.color_dict = {
             'Ciemnoniebieski': '#001f3f',
             'Jasnopoziomkowy': '#ff007f',
@@ -18,10 +18,11 @@ class Graph3D(tk.Frame):
             'Pomarańczowy': '#ff6300',
             'Jadeitowy': '#00A86B'
         }
-        self.marker = 'o'
+        self.markers = ['o', 'x', '+', '1', 'D', '^']
+        self.column = None
         # self.mount()
 
-    def mount(self):
+    def mount_normal(self):
         df = self.main_window.engine.get_dataset()
         x = df[self.data[0]]
         y = df[self.data[1]]
@@ -43,11 +44,67 @@ class Graph3D(tk.Frame):
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    def set_data(self, x, y, z, scatter_or_plot, color, marker):
-        self.color = color
-        self.marker = marker
+    def mount_with_class(self):
+        df = self.main_window.engine.get_dataset()
+        x = df[self.data[0]]
+        y = df[self.data[1]]
+        z = df[self.data[2]]
+
+        column_data = df[self.column].unique()
+
+        x_dict = {}
+        y_dict = {}
+        z_dict = {}
+
+        for class_data in column_data:
+            x_dict[class_data] = []
+            y_dict[class_data] = []
+            z_dict[class_data] = []
+
+        fig = Figure(figsize=(5, 4), dpi=100)
+        plot = fig.add_subplot(111, projection='3d')
+
+        for (x, y, z, class_data) in zip(df[self.data[0]].values, df[self.data[1]].values, df[self.data[2]].values, df[self.column].values):
+            x_dict[class_data].append(x)
+            y_dict[class_data].append(y)
+            z_dict[class_data].append(z)
+
+        if self.scatter_or_plot == 'punkty':
+            for idx, class_data in enumerate(column_data):
+                plot.scatter(x_dict[class_data], y_dict[class_data], z_dict[class_data], linewidth=2, color=self.colors[idx], marker=self.markers[idx], label=class_data)
+        else:
+            for idx, class_data in enumerate(column_data):
+                plot.plot(x_dict[class_data], y_dict[class_data], z_dict[class_data], linewidth=2, color=self.colors[idx], label=class_data)
+
+        plot.set_xlabel(self.data[0])
+        plot.set_ylabel(self.data[1])
+        plot.set_zlabel(self.data[2])
+        plot.legend()
+
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def set_data(self, x, y, z, scatter_or_plot, color_names, markers, column):
+        colors = []
+
+        for color_name in color_names:
+            colors.append(self.color_dict[color_name])
+
+        if colors:
+            self.colors = colors
+        if markers:
+            self.markers = markers
+
+        self.column = column
         self.scatter_or_plot = scatter_or_plot
         self.data = [x, y, z]
+
+    def mount(self):
+        if self.column == '':
+            self.mount_normal()
+        else:
+            self.mount_with_class()
 
     @property
     def main_window(self):
--- src/components/graphs/graphDialog3d.py
+++ src/components/graphs/graphDialog3d.py
@@ -22,6 +22,12 @@ class GraphDialog3d(tk.Toplevel):
         self.color_names_selected = []
         self.markers = ['o', 'x', '+', '1', 'D', '^']
         self.markers_selected = []
+        self.series_var = None
+        self.series = None
+        self.column_data_loaded = False
+        self.selected_column = None
+        self.class_cbx = None
+        self.class_var = None
 
         self.mount()
 
@@ -33,36 +39,82 @@ class GraphDialog3d(tk.Toplevel):
         self.scatter_or_plot.grid(row=0, column=0, sticky='nw')
 
         numeric_columns = self.df.select_dtypes(include=['int64', 'float64']).columns.tolist()
+        columns = self.df.columns.tolist()
 
-        for i in range(3):
+        for i, name in enumerate(['X', 'Y', 'Z']):
+            series_label = tk.Label(self, text=name)
+            series_label.grid(row=i + 1, column=0, sticky='nw', columnspan=2)
+
             self.vars[i] = tk.StringVar()
             combobox = ttk.Combobox(self, textvariable=self.vars[i],
                                     values=numeric_columns)
-            combobox.grid(row=i + 1, column=0, sticky='nw')
+            combobox.grid(row=i + 1, column=1, sticky='nw')
             combobox.set(numeric_columns[i])
             self.comboboxes.append(combobox)
 
-        color_var = tk.StringVar()
-        color_combobox = ttk.Combobox(self, textvariable=color_var,
-                                      values=self.color_names)
-        color_combobox.grid(row=4, column=0, sticky='nw')
-
-        self.color_names_selected.append(color_var)
-
-        marker_var = tk.StringVar()
-        marker_combobox = ttk.Combobox(self, textvariable=marker_var,
-                                       values=self.markers)
-        marker_combobox.grid(row=5, column=0, sticky='nw')
-
-        self.markers_selected.append(marker_var)
+        class_label = tk.Label(self, text="Wybierz klasę")
+        class_label.grid(row=5, column=0, sticky='nw', columnspan=3)
+        self.class_var = tk.StringVar()
+        self.class_cbx = ttk.Combobox(self, textvariable=self.class_var,
+                                      values=columns)
+        self.class_cbx.grid(row=6, column=0, sticky='nw')
 
         self.button = tk.Button(self, text="Dalej")
-        self.button.config(command=self.get_graph_params_and_destroy)
-        self.button.grid(row=len(numeric_columns) + 2, column=0, sticky="ne")
+        self.button.config(command=self.manage_state)
+        self.button.grid(row=7, column=0, sticky="ne")
+
+    def mount_2(self):
+        self.button.destroy()
+        self.button.update()
+        column_data = self.df[self.class_var.get()].unique()
+        column_data_len = len(column_data)
+
+        for i, data in enumerate(column_data):
+            series_label = tk.Label(self, text=data)
+            series_label.grid(row=i + 8, column=0, sticky='nw', columnspan=3)
+
+            color_var = tk.StringVar()
+            color_combobox = ttk.Combobox(self, textvariable=color_var,
+                                          values=self.color_names)
+            color_combobox.grid(row=i + 8, column=1, sticky='nw')
+            color_combobox.set(self.color_names[i % column_data_len])
+
+            self.color_names_selected.append(color_var)
+
+            marker_var = tk.StringVar()
+            marker_combobox = ttk.Combobox(self, textvariable=marker_var,
+                                           values=self.markers)
+            marker_combobox.grid(row=i + 8, column=2, sticky='nw')
+            marker_combobox.set(self.markers[i % column_data_len])
+
+            self.markers_selected.append(marker_var)
+
+        self.button = tk.Button(self, text="Dalej")
+        self.button.config(command=self.manage_state)
+        self.button.grid(row=len(column_data) + 9, column=0, sticky="ne")
+
+    def manage_state(self):
+        if not self.column_data_loaded and self.class_var.get() != '':
+            self.mount_2()
+            self.column_data_loaded = True
+        else:
+            self.get_graph_params_and_destroy()
 
     def get_graph_params_and_destroy(self):
+        colors = []
+
+        for color in self.color_names_selected:
+            if color.get() != '':
+                colors.append(color.get())
+
+        markers = []
+
+        for marker in self.markers_selected:
+            if marker.get() != '':
+                markers.append(marker.get())
+
         self.data_setter(self.vars[0].get(), self.vars[1].get(), self.vars[2].get(), self.scatter_or_plot_var.get(),
-                         self.color_names_selected[0].get(), self.markers_selected[0].get())
+                         colors, markers, self.class_var.get())
         self.master.center_panel.mount_graph_3d()
         self.destroy()
         self.update()
